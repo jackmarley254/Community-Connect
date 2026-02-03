86a4aa7362cbfd4b2551b5cd38ba8907b4224e07
jackmarley254 <engndiritu1@gmail.com>
2026-02-03 13:09:52 +0300

    Add root URL pattern to fix 404

--- community_connect/urls.py
+++ community_connect/urls.py
@@ -1,34 +1,16 @@
-"""
-URL configuration for community_connect project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-from property.views import dashboard_redirect_view  # <--- THIS IMPORT IS CRITICAL
+from property.views import dashboard_redirect_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    # THIS LINE FIXES THE BLANK SCREEN:
-    # It tells Django: "When at /home/, run the traffic cop function"
+    
+    # Redirect logged-in users
     path('home/', dashboard_redirect_view, name='home'),
-
-    # Splash Page
+    
+    # ROOT URL -> Goes to users/urls.py (which now has the splash page)
     path('', include('users.urls')),
-
-    # Auth & App
+    
     path('auth/', include('users.urls')),
     path('app/', include('property.urls')),
 ]
--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from . import views
 
-# Define the app namespace
 app_name = 'users'
 
 urlpatterns = [
+    # --- 1. THE MISSING LINK (Fixes 404 at /) ---
+    # This tells Django: "When the path is empty, show the Splash Page"
+    path('', views.splash_page_view, name='splash'),
+
+    # --- 2. Authentication ---
     path('login/', views.login_view, name='auth_login'),
     path('logout/', views.logout_view, name='auth_logout'),
 ]
-    # Note: 'home' is defined in the project's root urls.py
